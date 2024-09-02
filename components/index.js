7411122a985389fc616c8689cb40c436302a2d2d
zinghrAmit <141804407+zinghrAmit@users.noreply.github.com>
2024-09-02 20:36:58 +0530

    minor changes

--- components/index.js
+++ components/index.js
@@ -14,6 +14,7 @@ import FeatureOne from "./FeatureOne";
 import FeatureTwo from "./FeatureTwo";
 import FeatureThree from "./FeatureThree";
 import FeatureFour from "./FeatureFour";
+import { ThemeProvider } from "./Theme_Context/Theme";
 
 const comp = {
     Topbar,
@@ -31,7 +32,8 @@ const comp = {
     FeatureOne,
     FeatureTwo,
     FeatureThree,
-    FeatureFour
+    FeatureFour,
+    ThemeProvider
 };
 
 export default comp;
